862d25ce66c82bfe67003c30980356d8151c8903
vasgat <vasgat@gmail.com>
2024-11-22 16:18:37 +0100

    [BUILD] updating start script to run update_cached_data script before setting up the cronjob

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,8 @@
 # Start nginx in the background
 nginx -g "daemon off;" &
 
-# Start supercronic to run cron jobs
+# Run the script immediately for the first time
+ruby /fashionchecker/script/update_cached_data.rb
+
+# Start supercronic to run cron jobs (updating cached data every 10 mins)
 /usr/local/bin/supercronic /fashionchecker/cronfile
